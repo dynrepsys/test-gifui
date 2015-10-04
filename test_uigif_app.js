2559486b70843362de2660b9f2d8028fd8e69478
Jason Koziol <jason@42andpark.com>
2015-10-04 11:01:11 -0500

    initial version

---
+++ test_uigif_app.js
@@ -0,0 +1,30 @@
+var Gifsocket = require('gifsockets');
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+      var gifsocket = new Gifsocket({
+        // #GIFSOCKET-DIMENSIONS
+        width: 200,
+        height: 200
+      });
+  });
+}
