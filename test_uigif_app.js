619379178e3179ef366c0585ea8da22dbd62adc4
Jason Koziol <jason@42andpark.com>
2015-10-05 12:18:48 -0500

    file -> img stream

--- test_uigif_app.js
+++ test_uigif_app.js
@@ -19,7 +19,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-var Gifsocket = Meteor.npmRequire('gifsockets');
+var path = Meteor.npmRequire('path');
+var fs = Meteor.npmRequire('fs');
+//var Gifsocket = Meteor.npmRequire('gifsockets');
     /*
     FlowRouter.route('/', {
         name: "root",
@@ -35,16 +37,43 @@ var Gifsocket = Meteor.npmRequire('gifsockets');
         }
     });
     */
-Picker.route('/image.gif', function(params, req, res, next) {
-            console.log("image");
-});
+    Picker.route('/image.gif', function(params, req, res, next) {
+        console.log("image");
+        res.writeHead(200, {
+//            'zconnection': 'keep-alive',
+            'Content-type': 'image/jpeg'
+//            'ztransfer-encoding': 'chunked'
+        });
+        /*filePath = path.join("", 'one.png');
+        console.log(filePath);
+        fs.exists(filePath, function(exists){
+            console.log( exists )
+        });
+        console.log(__meteor_bootstrap__.__dirname);
+        console.log(path.resolve('.'));
+        */
+        var filePath = path.join(path.resolve('.').split('server')[0],'web.browser/app/img/one.png');
+        console.log(filePath);
+        //fs.exists(filePath, function(exists ){
+          //  console.log( exists );
+            //console.log( filePath );
+            //if(exists)
+                var data = fs.readFileSync(filePath);
+                res.write(data);
+            res.end();
+        console.log('done');
+        //});
+
+    });
 
   Meteor.startup(function () {
     // code to run on server at startup
+      /*
       var gifsocket = new Gifsocket({
         // #GIFSOCKET-DIMENSIONS
         width: 200,
         height: 200
       });
+      */
   });
 }
